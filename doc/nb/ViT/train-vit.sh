14fbe2a8a9b2eabf25edf990b21b3aa88e7c8e98
Delaney Cummins <110001366+delaney-cummins@users.noreply.github.com>
2023-12-06 16:23:49 -0500

    Update train-vit.sh

--- doc/nb/ViT/train-vit.sh
+++ doc/nb/ViT/train-vit.sh
@@ -18,4 +18,4 @@ export OMP_PROC_BIND=spread
 #run the application:
 #applications may perform better with --gpu-bind=none instead of --gpu-bind=single:1 
 module load pytorch/1.13.1
-srun -n 1 -c 128 --cpu_bind=cores -G 1 --gpu-bind=single:1 python /pscratch/sd/d/dcummins/vit/Training1.py
+srun -n 1 -c 128 --cpu_bind=cores -G 1 --gpu-bind=single:1 python /pscratch/sd/d/dcummins/vit/Training.py
